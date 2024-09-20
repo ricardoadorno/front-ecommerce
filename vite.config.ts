3b9c918ab0ae7fbc37729b4b086a81d30fba48d4
Ricardo Adorno <ricardo.castrorc1998@gmail.com>
2024-09-19 22:22:50 -0300

    fix: deploy

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/front-ecommerce',
+  base: '/front-ecommerce/',
   test: {
     environment: 'jsdom',
   },
